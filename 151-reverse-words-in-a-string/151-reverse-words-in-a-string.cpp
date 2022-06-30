80cfd9313a04488400c673b7149d92d65c192133
Sarthak srivastava <srk.lala2000@gmail.com>
2022-06-30 13:49:33 +0530

    Time: 24 ms (16.22%), Space: 20.7 MB (26.87%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int i=0;
-        int n=s.length();
-        string result;
-        while(i<n)
+        int i=0; // starting 
+        int n=s.length(); //storing length of string 
+        string result; // creating string for storing result
+        while(i<n) //keep traverse in string 
         {
-            while(i<n && s[i]==' ') i++;
-            if(i>=n) break;
-            int j=i+1;
-            while(j<n && s[j]!=' ') j++;
-            string sub=s.substr(i,j-i);
-            if(result.length()==0)
+            while(i<n && s[i]==' ') i++; // now find traversing the white spaces untilll you find a character 
+            if(i>=n) break; // checking the corner case  if string is empty
+            int j=i+1; //initialing variable for traversing after found  character
+            while(j<n && s[j]!=' ') j++; //keep traversing untill find space 
+            string sub=s.substr(i,j-i); //maintaining the substring until the white space 
+            if(result.length()==0) //checking for corner case if it is the first string we are storing in result 
             {
-                result=sub;
+                result=sub;// storing that substring in result 
             }
             else{
-                result=sub+" "+result;
+                result=sub+" "+result; // if it was not first substring that means result storing some other sting also so we need to add the new substring  before the older one 
             }
-            i=j+1;
+            i=j+1; // set i=j+1 for keep traversing 
         }
         return result;
     }
