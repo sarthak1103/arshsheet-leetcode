29038a8f568ba1f4f87377697975daba4da78fd8
Sarthak srivastava <srk.lala2000@gmail.com>
2022-06-30 14:28:59 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -1,33 +1,40 @@
 // { Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
-// User function Template for C++
-class Solution {
-  public:
-    string revStr(string S) {
-        string sol;
-        for(int i=S.length()-1;i>=0;i--)
-        {
-            sol.push_back(S[i]);
-            
-        }
-        return sol;
-    }
-};
 
-// { Driver Code Starts.
+string reverseWord(string str);
+
+
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string S;
-        cin >> S;
-        Solution ob;
-        cout << ob.revStr(S) << endl;
-    }
-    return 0;
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+  int n=str.length();
+  string res;
+  for(int i=n-1;i>=0;i--)
+  {
+      res.push_back(str[i]);
+  }
+  return res;
+}
