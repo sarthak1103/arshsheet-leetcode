5fd37521f2158720888771aa7841225e06b4f9d6
Sarthak srivastava <srk.lala2000@gmail.com>
2022-07-06 00:57:14 +0530

    Time: 200 ms (23.70%), Space: 67.7 MB (90.54%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max=INT_MIN;
+        int min=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            min=min+nums[i];
+            if(max<min)
+            {
+                max=min;
+            }
+            if(min<0)
+            {
+                min=0;
+            }
+        }
+        return max;
+        
+    }
+};
