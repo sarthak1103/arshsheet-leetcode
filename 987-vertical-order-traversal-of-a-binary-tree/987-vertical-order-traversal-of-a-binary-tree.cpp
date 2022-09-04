405631c096d83215d7ed423c88cf2fa0671f22ae
Sarthak srivastava <srk.lala2000@gmail.com>
2022-09-05 04:07:55 +0530

    Time: 10 ms (44.86%), Space: 13.7 MB (20.81%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,23 +12,34 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-          map<int,map<int,multiset<int>>> mp;
-        vector<vector<int>> ans;
-        queue<pair<TreeNode*,pair<int,int>>> q;
-        q.push({root,{0,0}});
-        while(!q.empty()){
-            root=q.front().first;
-            int x=q.front().second.first,y=q.front().second.second;q.pop();
-            mp[x][y].insert(root->val);
-            if(root->left) q.push({root->left,{x-1,y+1}});
-            if(root->right) q.push({root->right,{x+1,y+1}});
+        map<int,map<int,multiset<int> > > nodes;
+        queue<pair<TreeNode*,pair<int,int> > > q;
+       // vector<vector<int> > ans;
+        // if(root==NULL)
+        //     return ans;
+        q.push(make_pair(root,make_pair(0,0)));
+        while(!q.empty())
+        {
+            pair<TreeNode*,pair<int,int> > temp=q.front();
+            q.pop();
+            TreeNode *FirstNode=temp.first;
+            int hd=temp.second.first;
+            int level=temp.second.second;
+            nodes[hd][level].insert(FirstNode->val);
+            if(FirstNode->left)
+                q.push(make_pair(FirstNode->left,make_pair(hd-1,level+1)));
+            if(FirstNode->right)
+                q.push(make_pair(FirstNode->right,make_pair(hd+1,level+1)));
         }
-        for(auto x:mp){
-            vector<int> col;
-            for(auto y:x.second){
-                for(auto ele:y.second) col.push_back(ele);
+        vector<vector<int> >ans;
+        for(auto i:nodes)
+        {
+            vector<int> cols;
+            for(auto p:i.second)
+            {
+                cols.insert(cols.end(),p.second.begin(),p.second.end());
             }
-            ans.push_back(col);
+            ans.push_back(cols);
         }
         return ans;
     }
