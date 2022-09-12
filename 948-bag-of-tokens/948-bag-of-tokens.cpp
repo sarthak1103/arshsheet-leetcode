032f71ba1bbe477fb963383047bd5c52a2c4fe3c
Sarthak srivastava <srk.lala2000@gmail.com>
2022-09-13 03:07:19 +0530

    Time: 11 ms (53.55%), Space: 10.5 MB (94.43%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(),tokens.end());   
+    int t_size = size(tokens); 
+    int gain = 0;               
+    int score = 0, max_score = 0;
+    int spend = t_size-1;       
+    
+    while(gain<=spend){
+        
+        if(power >= tokens[gain]){  
+            score+=1;
+            max_score = std::max(max_score,score); 
+            power-=tokens[gain];
+            gain++;
+        }
+        
+        else if(score >= 1){        
+            power += tokens[spend];
+            score -= 1;
+            spend--;
+        }
+        else
+            break;                  
+    }
+    
+    return max_score;
+    
+    }
+};
