54f53e15cbdfeecffa96605acd10cd94c9384664
Sarthak srivastava <srk.lala2000@gmail.com>
2022-09-22 16:38:10 +0530

    Time: 32 ms (9.88%), Space: 5.8 MB (78.62%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-     
-        if(n<=1) 
+        if(n<=1)
             return n;
-          
-       return fib(n-1) + fib(n-2); //   
-        
+        return fib(n-1)+fib(n-2);
     }
 };
-
-
-// to do with dp follow three steps 
-
-// 1-create a vector of dp initialize it with -1
-// 2- check the base case 
-// 3- again check  if the we already cmputed the value 
-// 4- store whatever computed  in vector of dp
