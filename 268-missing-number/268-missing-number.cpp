ab44b528907f99381414b132af46bc329858bf34
Sarthak srivastava <srk.lala2000@gmail.com>
2022-09-27 18:06:57 +0530

    Time: 62 ms (9.61%), Space: 18.1 MB (20.24%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        int res=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans+=nums[i];
+            res=(nums.size()*(nums.size()+1))/2-ans;
+        }
+        return res;
+    }
+};
