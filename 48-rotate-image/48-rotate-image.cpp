209efca50882fd33cae2e3203cff0fe7f75f021f
Sarthak srivastava <srk.lala2000@gmail.com>
2022-10-06 10:05:04 +0530

    Time: 4 ms (43.46%), Space: 7.1 MB (73.70%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();// first we wll store the size of orignal matrix
-        for(int i=0;i<n;i++)// now traverse in  each vector of matrix
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<i;j++)// traversing elemnts in each matrix
+            for(int j=0;j<i;j++)
             {
-                swap(matrix[i][j],matrix[j][i]);// swaping th elements of rows into col and col into rows
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
         for(int i=0;i<n;i++)
         {
-            reverse(matrix[i].begin(),matrix[i].end());// finaly revesing the each vector(row) in matrix
+            reverse(matrix[i].begin(),matrix[i].end());
         }
-        
     }
 };
